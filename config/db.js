c3178e85754fd1a867dd685037adb77ccda652a0
Elias Eskelinen <104145838+EskelinenElias@users.noreply.github.com>
2024-01-15 19:24:40 +0200

    Some fixes

--- config/db.js
+++ config/db.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 const address = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/testdb';
 
 // connect to the database
-console.log('Connecting to MongoDB...')
+console.log(`Connecting to MongoDB... ${address}`)
 mongoose.connect(address);
 mongoose.promise = Promise; 
 const db = mongoose.connection;
