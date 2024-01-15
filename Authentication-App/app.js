0e41172443bc03784525c27420f86827239e2790
Elias Eskelinen <104145838+EskelinenElias@users.noreply.github.com>
2024-01-15 17:58:21 +0200

    Fixed registering new user

--- Authentication-App/app.js
+++ Authentication-App/app.js
@@ -21,8 +21,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', usersRouter);
 
+/*
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -38,5 +39,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+*/
 module.exports = app;
--- Authentication-App/routes/users.js
+++ Authentication-App/routes/users.js
@@ -2,25 +2,30 @@ var express = require('express');
 var router = express.Router();
 const User = require('../models/user');
 
+// User registration route
+router.post('/api/user/register', async (req, res) => {
+    try {
+        console.log("Registering a new user...")
+        const { email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            console.log('New user not registered; email already in use.')
+            return res.status(403).send('This email is already registered.');
+        }
+        const user = new User({ email, password });
+        await user.save();
+        console.log('New user registered.')
+        res.send('Successfully registered user'); 
+    } catch (error) {
+        console.error('An error occurred while attempting to register a new user:', error);
+        res.status(500).send("Failed to register user.");
+    }
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-// User registration route
-router.post('/api/user/register', async (req, res) => {
-    try {
-        const { username, password } = req.body;
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).send('This email is already registered.');
-        }
-        const user = new User({ username, password });
-        await user.save()
-        res.send('Successfully registered user.') 
-    } catch (error) {
-        res.status(500).send("Failed to register user.")
-    }
-});
 
 module.exports = router;
